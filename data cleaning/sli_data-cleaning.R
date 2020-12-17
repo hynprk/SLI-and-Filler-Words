4dc5100fca7a24bfb31db4dbe7bc10ee3838b0b9
Hyo-Eun Park <61765475+hynprk@users.noreply.github.com>
2020-12-17 22:09:46 +0900

    omitting age above 12

--- data cleaning/sli_data-cleaning.R
+++ data cleaning/sli_data-cleaning.R
@@ -44,16 +44,12 @@ cleaned <- cleaned %>% na.omit() # none!
 ### Rounding Age to two decimal places
 cleaned <- cleaned %>% 
   mutate(age_years = round(age_years, 2))
+
 ### Changing name of variable
 names(cleaned)[names(cleaned) == 'age_years'] <- 'age'
 
-### Dividing into three different groups ###
-cleaned <- cleaned %>%
-  mutate(age_group = 
-           ifelse(
-             4 <= age & age <= 6.99, "younger",
-             ifelse(6.99 < age & age <= 10.99,"middle",
-                    "older")))
+### Omitting those above the age of 12 ###
+cleaned <- cleaned %>% filter(age < 13)
 
 ### New dummy variable for treatment group ###
 cleaned <- cleaned %>% 
